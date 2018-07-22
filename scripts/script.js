afc3bedd941e786026a22672f679ac694bd2a7a6
StarNeit <koneit@outlook.com>
2018-07-22 16:55:31 +0200

    update

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,10 @@ $(window).resize( changePlaceholder ).trigger('resize');
 
 
 $(document).ready(function(){
-
+    $(".orders_menu .flex-sm-1").on("click", function () {
+        $(".orders_menu .flex-sm-1").removeClass("active");
+        $(this).addClass("active");
+    });
 });
 
 $(window).scroll(function () {
