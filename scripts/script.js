fbf9d18492a7e82c317c276255c46c1c2d998755
StarNeit <koneit@outlook.com>
2018-07-22 11:35:38 +0200

    init

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function changePlaceholder() {
+
+}
+
+$(window).resize( changePlaceholder ).trigger('resize');
+
+
+$(document).ready(function(){
+
+});
+
+$(window).scroll(function () {
+
+    }
+);
